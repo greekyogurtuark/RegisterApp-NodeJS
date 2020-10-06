8033af91ce13c339866bd8c20b4d1c80f85ad7a0
Trentonb182 <43834960+Trentonb182@users.noreply.github.com>
2020-10-06 16:09:15 -0500

    started main menu script, and edited mainMenu.ejs

---
+++ public/scripts/mainMenu.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+	// TODO: Anything you want to do when the page is loaded?
+  document.getElementsById("transaction").addEventListener("click",showTransaction);
+  document.getElementsById("products").addEventListener("click",showProducts);
+  document.getElementsById("employee").addEventListener("click",showEmployee);
+  document.getElementsById("salesReport").addEventListener("click",show_salesReport);
+  document.getElementsById("cashierReport").addEventListener("click",show_cashierReport);
+});
+
+
+function showTransaction(){
+  alert(“Functionality has not yet been implemented.” );
+}
+function showProducts(){
+  window.location.assign(
+        "/productDetail/");
+}
+function showEmployee(){
+  window.location.assign(
+        "/employeeDetail/");
+}
+function show_salesReport(){
+  alert(“Functionality has not yet been implemented.” );
+}
+function show_cashierReport(){
+  alert(“Functionality has not yet been implemented.” );
+}
