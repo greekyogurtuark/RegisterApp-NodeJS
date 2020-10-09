51acdaea4a253762e3cfab6c37e8363610cd4251
Trentonb182 <43834960+Trentonb182@users.noreply.github.com>
2020-10-08 21:58:19 -0500

    fixed syntax to be the correct way for it to be compatible with main menu.

--- src/routes/mainMenuRoutes.ts
+++ src/routes/mainMenuRoutes.ts
@@ -1,10 +1,10 @@
 
 import express from "express";
 import { RouteLookup } from "../controllers/lookups/routingLookup";
-import * as ProductListingRouteController from "../controllers/mainMenuRouteController";
+import * as MainMenuRouteController from "../controllers/mainMenuRouteController";
 
-function productListingRoutes(server: express.Express) {
-server.get(RouteLookup.MainMenu, MainMenuRouteController.start);
+function mainMenuRoutes(server: express.Express) {
+    server.get(RouteLookup.MainMenu, MainMenuRouteController.start);
 }
 
 module.exports.routes = mainMenuRoutes;
