185596ef77b99c8f5622a5924d8f046ec42abf71
noajam <nrj006@uark.edu>
2020-10-11 04:43:32 -0500

    hotfix

--- public/scripts/signIn.js
+++ public/scripts/signIn.js
@@ -1,36 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // TODO: Anything you want to do when the page is loaded?
-})
+document.addEventListener("DOMContentLoaded", () => {
+	const employeeIdEditElement = getEmployeeIdEditElement();
+	employeeIdEditElement.focus();
+	employeeIdEditElement.select();
+});
 
-function validateForm () {
-  var eID = document.getElementsByClassName('inputContent').eID
-  var pass = document.getElementsByClassName('inputContent').pass
-  var fail = false
-<<<<<<< HEAD
+function validateForm() {
+	const employeeIdEditElement = getEmployeeIdEditElement();
+	if (isNaN(Number(employeeIdEditElement.value))
+		|| (Number(employeeIdEditElement.value) <= 0)) {
 
-  if (eID == '' || isNaN(eID)) {
-    alert('Employee ID must be nonempty and a sequence of numbers: ' + eID)
-    fail = true
-  }
-  if (pass == '') {
+		alert("Please provide a valid employee ID.");
 
-    alert('Password must be nonempty.')
-    fail = true
-  }
+		employeeIdEditElement.focus();
+		employeeIdEditElement.select();
 
-=======
-  if (eID == '' || isNaN(eID)) {
-    alert('Employee ID must be nonempty and a sequence of numbers.')
-    fail = true
-  }
-  if (pass == '') {
-    alert('Password must be nonempty.')
-    fail = true
-  }
+		return false;
+	}
 
->>>>>>> 7d2dfaae46eae65b63fe74c545e89664ea3136ed
-  if (fail) {
-    return false
-  }
-  return true
+	const passwordEditElement = getPasswordEditElement();
+	if ((passwordEditElement.value == null)
+		|| (passwordEditElement.value.trim() === "")) {
+
+		alert("Please provide a valid password. It may not be blank.");
+
+		passwordEditElement.focus();
+		passwordEditElement.select();
+
+		return false;
+	}
+
+	return true;
 }
+
+//Getters and setters
+function getPasswordEditElement() {
+	return document.getElementById("password");
+}
+
+function getEmployeeIdEditElement() {
+	return document.getElementById("employeeId");
+}
+//End getters and setters
